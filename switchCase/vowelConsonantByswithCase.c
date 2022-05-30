e1e81cf99764a9d1262c0a39271a9ebfef029d5c
Tunvir Montasir <shovontunvir43@gmail.com>
2022-05-30 22:03:30 +0600

    vowel consonent by switch case

---
+++ switchCase/vowelConsonantByswithCase.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    char ch;
+    printf("enter a word :");
+    scanf("%c", &ch);
+    switch (ch)
+    {
+    case 'a' :
+    case 'e' :
+    case 'i' :
+    case 'o' :
+    case 'u' :
+    case 'A' :
+    case 'E' :
+    case 'I' :
+    case 'O' :
+    case 'U' :
+        printf("%c is vowel", ch);
+        break;
+
+        
+    
+    default:
+        printf("%c is consonant", ch);
+        break;
+    }
+}
