5789386a922f52fb5bbf75058d27970e33b54b83
Tunvir Montasir <shovontunvir43@gmail.com>
2022-05-30 21:55:19 +0600

    swith case basic

--- switchCase/sitchCaseBasic.c
+++ switchCase/sitchCaseBasic.c
@@ -1,4 +1,6 @@
 //core is : switch case break default
+//defaul block use every where
+// case not same 
 #include <stdio.h>
 
 int main(){
@@ -10,20 +12,26 @@ int main(){
     {
         case 0: 
             printf("it's zero \n");
+            break;
         case 1: 
             printf("it's one \n");
+            break;
         case 2: 
             printf("it's two \n");
+            break;
         case 3: 
             printf("it's three \n");
         case 4: 
             printf("it's four \n");
         case 5: 
             printf("it's five \n");
+            break;
         case 6: 
             printf("it's six \n");
+            break;
         
         default:
             printf("number is no match");
+            break;
     }
 }
