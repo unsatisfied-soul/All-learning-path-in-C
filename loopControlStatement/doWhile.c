2dc64c8cf6018728b78d08dc8fbe44000cae53d6
Tunvir Montasir <shovontunvir43@gmail.com>
2022-06-02 09:57:38 +0600

    do while

---
+++ loopControlStatement/doWhile.c
@@ -0,0 +1,11 @@
+//condition add in while bracket
+//update add enter the loop
+#include<stdio.h>
+
+int main(){
+    int i =1;
+    do {
+        printf("i love C programming\n");
+        i++;
+    }while(i<=10);
+}
