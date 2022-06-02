0aa5073e317b594f9cbb80288f1769e7d698bbc5
Tunvir Montasir <shovontunvir43@gmail.com>
2022-06-02 09:55:22 +0600

    while loop

---
+++ loopControlStatement/whileLoop.c
@@ -0,0 +1,12 @@
+//no initaial value add in while loop
+//just condition add
+//update add enter the loop
+#include<stdio.h>
+
+int main(){
+    int i =1;
+    while (i <=10){
+        printf("i love c programming\n");
+        i++;
+    }
+}
