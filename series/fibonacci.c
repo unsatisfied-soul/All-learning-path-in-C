bc552e5e8792e9328f2c0858ec927c1f7b16a13f
Tunvir Montasir <shovontunvir43@gmail.com>
2022-06-15 13:13:41 +0600

    fibonacci

---
+++ series/fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numbValue,i,firstNumber=0,secondNumber=1,fibonacciNumber;
+    printf("enter a number :");
+    scanf("%d", &numbValue);
+    printf("%d, %d,", firstNumber,secondNumber);
+    for(i=0; i<=numbValue; i++){
+        if(i==0)
+        fibonacciNumber = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = fibonacciNumber;
+        printf("%d,", fibonacciNumber);
+    }
+}
