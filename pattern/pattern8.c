fc447f1b888013f14274b14c3cb6f705d1e6a6b8
Tunvir Montasir <shovontunvir43@gmail.com>
2022-06-19 12:39:47 +0600

    pattern8

---
+++ pattern/pattern8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int num,row,col;
+    printf("enter a number:");
+    scanf("%d", &num);
+    for(row=1; row<num; row++){
+        for(col=1; col<=row; col++){
+            printf("%d",col);
+        }
+        printf("\n");
+    }
+    for(row=num; row>=1; row--){
+        for(col=1; col<=row; col++){
+            printf("%d", col);
+        }
+        printf("\n");
+    }
+}
