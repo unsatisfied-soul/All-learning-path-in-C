5f62d8c82e2d79c0eb9bd926645fe23170dbf9e6
Tunvir Montasir <shovontunvir43@gmail.com>
2022-06-20 12:19:52 +0600

    patter13

---
+++ pattern/pattern13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num,row,col;
+    printf("enter a number:");
+    scanf("%d", &num);
+    for(row=num;row>=1;row--){
+        for(col=row;col>=1;col--){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
