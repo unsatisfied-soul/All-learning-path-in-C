78ffbcda89f4a1d8ab9fea79689971882334f923
Tunvir Montasir <shovontunvir43@gmail.com>
2022-07-02 11:02:59 +0600

    basic string

---
+++ string/basicStr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char name[8];
+    name[0]='S';
+    name[1]='h';
+    name[2]='o';
+    name[3]='v';
+    name[4]='o';
+    name[5]='n';
+    name[6]='\0';
+    printf("%s",name);
+}
